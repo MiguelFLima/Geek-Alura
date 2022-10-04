1d2134e7c6150d11f053296eecf793643d8a646e
Miguel <miguel2014ssa@gmail.com>
2022-10-04 11:19:27 -0300

    listaProduto responsivo

--- components/ListaProdutos/ListaProdutos.js
+++ components/ListaProdutos/ListaProdutos.js
@@ -6,16 +6,16 @@ import starwars from "../../Dados/starwars";
 
 function ListaProdutos() {
   return (
-    <div className="xl:h-[942px] xl:w-[100%] flex justify-center items-center xl:mt-[90px]">
-      <div className="w-[80%]">
-        <div className="flex justify-between items-center w-[100%] xl:mb-[50px] xl:mt-[50px]">
-          <h1 className="font-bold text-lg">Todos os Produtos</h1>
-          <button className="bg-blue-700 text-white text-sm flex justify-center items-center xl:w-[164px] xl:h-[40px] xl:p-4">
+    <div className="xl:h-[942px] xl:w-[100%] flex justify-center items-center xl:mt-[90px] xl:flex-row flex-col">
+      <div className="xl:w-[80%] w-[70%] md:w-[70%]">
+        <div className="flex justify-between items-center mt-4 flex-col xl:flex-row md:flex-row w-[100%] mb-6 xl:mb-[50px] xl:mt-[50px] md:mb-[60px] md:mt-[30px]">
+          <h1 className="font-bold text-lg mb-4">Todos os Produtos</h1>
+          <button className="bg-blue-700 text-white p-2 mb-2 text-sm flex justify-center items-center xl:w-[164px] md:p-2 xl:h-[40px] xl:p-4">
             Adicionar Produto
           </button>
         </div>
 
-        <div className='flex w-full flex-wrap items-center justify-between gap-10 mb-6'>
+        <div className='flex w-full flex-wrap items-center md:justify-between xl:justify-between justify-center gap-10 mb-6'>
           {starwars.map((produto) => (
             <div key={produto.id}>
               <div className='xl:w-[176px] xl:h-[174px]'>
@@ -29,7 +29,7 @@ function ListaProdutos() {
       </div>
 
 
-      <div className="flex w-full flex-wrap items-center justify-between gap-10 mb-6">
+      <div className="flex w-full flex-wrap items-center md:justify-between xl:justify-between justify-center gap-10 mb-6">
           {consoles.map((produto) => (
             <div key={produto.id}>
               <div className="xl:w-[176px] xl:h-[174px]">
@@ -48,7 +48,7 @@ function ListaProdutos() {
           ))}
         </div>
 
-        <div className='flex w-full flex-wrap items-center justify-between gap-10 mb-10 xl:mb-[120px]'>
+        <div className='flex w-full flex-wrap items-center md:justify-between xl:justify-between justify-center gap-10 mb-10 xl:mb-[120px]'>
           {diversos.map((produto) => (
             <div key={produto.id}>
               <div className='xl:w-[176px] xl:h-[174px]'>
